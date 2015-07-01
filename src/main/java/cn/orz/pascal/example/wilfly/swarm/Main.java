212ae9a762397997d179778e6701b445e42ef8bf
koduki <koduki@nebula>
2015-07-01 23:04:38 +0900

    modify packaging method.

--- src/main/java/cn/orz/pascal/example/wilfly/swarm/Main.java
+++ src/main/java/cn/orz/pascal/example/wilfly/swarm/Main.java
@@ -7,11 +7,7 @@ package cn.orz.pascal.example.wilfly.swarm;
 
 import cn.orz.pascal.example.wilfly.swarm.api.EmployeeResource;
 import cn.orz.pascal.example.wilfly.swarm.api.JaxRsApplication;
-import cn.orz.pascal.example.wilfly.swarm.domain.Employee;
-import cn.orz.pascal.example.wilfly.swarm.domain.EmployeeService;
 import org.wildfly.swarm.container.Container;
-import org.wildfly.swarm.container.DefaultWarDeployment;
-import org.wildfly.swarm.container.WarDeployment;
 import org.wildfly.swarm.jaxrs.JAXRSDeployment;
 
 /**
@@ -27,8 +23,7 @@ public class Main {
         JAXRSDeployment deployment = new JAXRSDeployment(container);
         deployment.addResource(JaxRsApplication.class);
         deployment.addResource(EmployeeResource.class);
-        deployment.getArchive().addClasses(Employee.class);
-        deployment.getArchive().addClasses(EmployeeService.class);
+        deployment.getArchive().addPackage("cn.orz.pascal.example.wilfly.swarm.domain");
 
         container.start().deploy(deployment);
 
